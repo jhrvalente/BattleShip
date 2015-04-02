3eee57a187001a700fb868c70ee84316b62bca4a
joao801 <joaohenrique90@sapo.pt>
2015-04-02 16:14:46 +0100

    Gamewindow headers.

---
+++ window.h
@@ -0,0 +1,9 @@
+void createWindow(void);
+void setSize(int, int);
+void setBorderWidth(int);
+void setTitle(char *);
+void setPosition(GtkWindowPosition);
+void placeBoards(void);
+void placeButtons(void);
+void placeTextfield(void);
+void placeConsole(void);
