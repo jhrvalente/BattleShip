c9062777cd27b86cdbc0632b9278f820c879fb31
iTitou <moiandme@gmail.com>
2015-04-06 23:09:50 +0200

    [add] Terminal init sequence (better debug output)

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 #include "window.h"
 #include "board.h"
 #include "events.h"
+#include "termInit.h"
 
 int main(int argc, char **argv){
+  initTerm();
   gtk_init(&argc, &argv);
   createWindow();
   gtk_main();
