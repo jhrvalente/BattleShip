f3644b6563d726aedb27d862649b00bb024fd3d5
joao801 <joaohenrique90@sapo.pt>
2015-04-09 22:16:46 +0100

    Global variables added.

--- window.h
+++ window.h
@@ -1,3 +1,12 @@
+extern GtkWidget *btn_destroyer;
+extern GtkWidget *img_destroyer;
+extern GtkWidget *btn_submarine;
+extern GtkWidget *img_submarine;
+extern GtkWidget *btn_battleship;
+extern GtkWidget *btn_cruiser;
+extern GtkWidget *btn_carrier;
+extern GtkWidget *btn_clear;
+
 void createWindow(void);
 void setSize(int, int);
 void setBorderWidth(int);
